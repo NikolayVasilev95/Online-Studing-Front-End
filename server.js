b27000f65eb06c028df816f56470188add7d7bd3
Nikolay Vasilev <metalistite_two@abv.bg>
2019-12-06 12:21:55 +0200

    ServerConfig

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/OnlyneStuding'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/OnlyneStuding/index.html'));
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
